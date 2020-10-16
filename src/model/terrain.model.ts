4dbc92d9b12b474188829c5122b06f9a60c098df
AmiralBl3ndic <camille.briand@efrei.net>
2020-10-16 11:37:54 +0200

    refactor(Terrain): refactor parseLine

--- src/model/terrain.model.ts
+++ src/model/terrain.model.ts
@@ -38,116 +38,25 @@ class Terrain {
     throw new Error('Not implemented');
   }
 
-  // eslint-disable-next-line no-unused-vars
+  /**
+   * Parse data from a line and get the right data structure from it
+   * @param terrainLine Line to parse
+   */
   private static parseLine(terrainLine: string): Terrain | Tile[] | Adventurer {
-    const [lineType, ...terrainData] = terrainLine.trim().split(' - ');
+    const [lineType, ...lineData] = terrainLine.trim().split(' - ');
 
     switch (lineType) {
-      case 'C': {
-        if (terrainData.length < 2)
-          throw new Error('Too few information for terrain definition');
+      case 'C':
+        return Terrain.parseTerrain(lineData);
 
-        if (terrainData.length > 2)
-          throw new TerrainError('Too much information for terrain definition');
+      case 'T':
+        return Terrain.parseTreasures(lineData);
 
-        const [width, height] = terrainData.map((_) => Number(_));
+      case 'M':
+        return [Terrain.parseMountain(lineData)];
 
-        if (![width, height].every(Number.isInteger)) {
-          throw new TerrainError('Unparsable data for terrain definition');
-        }
-
-        if (width <= 0 || height <= 0) {
-          throw new TerrainError('Terrain cannot have 0 height or width');
-        }
-
-        return new Terrain(width, height);
-      }
-
-      case 'T': {
-        if (terrainData.length < 3)
-          throw new TerrainError(
-            'Too few information for tile of type treasure',
-          );
-
-        if (terrainData.length > 3)
-          throw new TerrainError(
-            'Too much information for tile of type treasure',
-          );
-
-        const [x, y, quantity] = terrainData.map((_) => Number(_));
-
-        if ([x, y, quantity].some((_) => !Number.isInteger(_) || _ < 0)) {
-          throw new TerrainError(
-            'Width, height and quantity must be >= 0 for a treasure tile',
-          );
-        }
-
-        return new Array<Tile>(quantity).fill({
-          x,
-          y,
-          type: 'treasure',
-        });
-      }
-
-      case 'M': {
-        if (terrainData.length < 2)
-          throw new TerrainError(
-            'Too few information for tile of type mountain',
-          );
-
-        if (terrainData.length > 2)
-          throw new TerrainError(
-            'Too much information for tile of type mountain',
-          );
-
-        const [x, y] = terrainData.map((_) => Number(_));
-
-        if ([x, y].some((_) => !Number.isInteger(_) || _ < 0)) {
-          throw new TerrainError(
-            'Width and height must be >= 0 for a mountain tile',
-          );
-        }
-
-        return [
-          {
-            x,
-            y,
-            type: 'mountain',
-          },
-        ];
-      }
-
-      case 'A': {
-        if (terrainData.length < 5)
-          throw new TerrainError('Too few information for adventurer');
-
-        if (terrainData.length > 5)
-          throw new TerrainError('Too much information for adventurer');
-
-        const adventurerName = terrainData[0];
-        const initialX = Number.parseInt(terrainData[1], 10);
-        const initialY = Number.parseInt(terrainData[2], 10);
-
-        if (!/[NSEO]/.test(terrainData[3]))
-          throw new AdventurerError('Unparsable orientation');
-
-        const orientation: Orientation = parseOrientation(terrainData[3]);
-
-        if (!/^[ADG]+$/.test(terrainData[4]))
-          throw new AdventurerError(
-            `Invalid actions/directions sequence for adventurer ${adventurerName}: ${terrainData[4]}`,
-          );
-
-        const actions: Action[] = terrainData[4].split('').map(parseAction);
-
-        return new Adventurer(
-          adventurerName,
-          initialX,
-          initialY,
-          orientation,
-          actions,
-        );
-      }
+      case 'A':
+        return Terrain.parseAdventurer(lineData);
 
       default: {
         throw new TerrainError(
@@ -155,6 +64,118 @@ class Terrain {
         );
       }
     }
+  }
+
+  /**
+   * Parse data from a terrain line
+   * @param terrainData Line elements to parse terrain data from
+   */
+  private static parseTerrain(terrainData: string[]): Terrain {
+    if (terrainData.length < 2)
+      throw new Error('Too few information for terrain definition');
+
+    if (terrainData.length > 2)
+      throw new TerrainError('Too much information for terrain definition');
+
+    const [width, height] = terrainData.map((_) => Number(_));
+
+    if (![width, height].every(Number.isInteger)) {
+      throw new TerrainError('Unparsable data for terrain definition');
+    }
+
+    if (width <= 0 || height <= 0) {
+      throw new TerrainError('Terrain cannot have 0 height or width');
+    }
+
+    return new Terrain(width, height);
+  }
+
+  /**
+   * Parse data from a treasure line
+   * @param treasureData Line elements to parse treasure data from
+   */
+  private static parseTreasures(treasureData: string[]): Tile[] {
+    if (treasureData.length < 3)
+      throw new TerrainError('Too few information for tile of type treasure');
+
+    if (treasureData.length > 3)
+      throw new TerrainError('Too much information for tile of type treasure');
+
+    const [x, y, quantity] = treasureData.map((_) => Number(_));
+
+    if ([x, y, quantity].some((_) => !Number.isInteger(_) || _ < 0)) {
+      throw new TerrainError(
+        'Width, height and quantity must be >= 0 for a treasure tile',
+      );
+    }
+
+    return new Array<Tile>(quantity).fill({
+      x,
+      y,
+      type: 'treasure',
+    });
+  }
+
+  /**
+   * Parse data from a mountain line
+   * @param mountainData Line elements to parse mountain data from
+   */
+  private static parseMountain(mountainData: string[]): Tile {
+    if (mountainData.length < 2)
+      throw new TerrainError('Too few information for tile of type mountain');
+
+    if (mountainData.length > 2)
+      throw new TerrainError('Too much information for tile of type mountain');
+
+    const [x, y] = mountainData.map((_) => Number(_));
+
+    if ([x, y].some((_) => !Number.isInteger(_) || _ < 0)) {
+      throw new TerrainError(
+        'Width and height must be >= 0 for a mountain tile',
+      );
+    }
+
+    return {
+      x,
+      y,
+      type: 'mountain',
+    };
+  }
+
+  /**
+   * Parse data from an adventurer line
+   * @param adventurerData Line elements to parse adventurer data from
+   */
+  private static parseAdventurer(adventurerData: string[]): Adventurer {
+    if (adventurerData.length < 5)
+      throw new TerrainError('Too few information for adventurer');
+
+    if (adventurerData.length > 5)
+      throw new TerrainError('Too much information for adventurer');
+
+    const adventurerName = adventurerData[0];
+    const initialX = Number.parseInt(adventurerData[1], 10);
+    const initialY = Number.parseInt(adventurerData[2], 10);
+
+    if (!/[NSEO]/.test(adventurerData[3]))
+      throw new AdventurerError('Unparsable orientation');
+
+    const orientation: Orientation = parseOrientation(adventurerData[3]);
+
+    if (!/^[ADG]+$/.test(adventurerData[4]))
+      throw new AdventurerError(
+        `Invalid actions/directions sequence for adventurer ${adventurerName}: ${adventurerData[4]}`,
+      );
+
+    const actions: Action[] = adventurerData[4].split('').map(parseAction);
+
+    return new Adventurer(
+      adventurerName,
+      initialX,
+      initialY,
+      orientation,
+      actions,
+    );
   }
 }
 

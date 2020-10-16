3c6ad51ed5c0b9d0769cd1fceb475ce430d4a399
AmiralBl3ndic <camille.briand@efrei.net>
2020-10-16 14:36:45 +0200

    feat(Terrain): implement parse method

--- src/model/terrain.model.ts
+++ src/model/terrain.model.ts
@@ -4,6 +4,7 @@ import Tile from '@/model/tile.model';
 import Action, { parseAction } from '@/model/action.enum';
 import Adventurer from '@/model/adventurer.model';
 import Orientation, { parseOrientation } from '@/model/orientation.enum';
+import LineType from '@/model/line-type.enum';
 
 /**
  * Model to represent and hold the map's data
@@ -17,6 +18,8 @@ class Terrain {
 
   treasures: Tile[] = [];
 
+  adventurers: Adventurer[] = [];
+
   constructor(w: number, h: number) {
     if (w <= 0 || h <= 0) {
       throw new Error(
@@ -28,15 +31,96 @@ class Terrain {
     this.height = h;
   }
 
-  // eslint-disable-next-line no-unused-vars
   static parse(terrainString: string): Terrain {
-    throw new Error('Not implemented');
+    // Separate data at the beginning so that the file can be read in any order
+    let terrain: Terrain | null = null;
+    const mountains: Tile[] = [];
+    const treasures: Tile[] = [];
+    const adventurers: Adventurer[] = [];
+
+    const linesToParse = terrainString
+      .split('\n')
+      .map((_) => _.trim()) // Get rid of spaces before and after interesting data
+      .filter((_) => _.length && !_.startsWith('#')); // Get rid of empty lines && comments lines
+
+    // Parse each line, a for-of loop would have been better optimized, but eslint complies if I use it
+    linesToParse.forEach((line) => {
+      const parsed = Terrain.parseLine(line);
+
+      // Must check type of returned object
+      if (parsed instanceof Terrain) {
+        if (terrain)
+          throw new TerrainError('Mismatching terrain dimensions data');
+        terrain = parsed;
+      } else if (parsed instanceof Array) {
+        if (parsed[0].type === 'mountain') {
+          // Check if no mountain already occupies this spot
+          if (mountains.some((m) => m.x === parsed[0].x && m.y === parsed[0].y))
+            throw new TerrainError('Multiple mountains on same tile');
+
+          // Check if no treasure already occupies this spot
+          if (treasures.some((t) => t.x === parsed[0].x && t.y === parsed[0].y))
+            throw new TerrainError('Treasure on a mountain tile');
+
+          // Check if no aventurer already occupies this spot
+          if (
+            adventurers.some((a) => a.x === parsed[0].x && a.y === parsed[0].y)
+          )
+            throw new TerrainError('Adventurer starts on a mountain');
+
+          mountains.push(parsed[0]);
+        } else {
+          // Check if no mountain already occupies this spot
+          if (mountains.some((m) => m.x === parsed[0].x && m.y === parsed[0].y))
+            throw new TerrainError('Treasure on a mountain tile');
+
+          treasures.push(...parsed);
+        }
+      } else {
+        // A terrain is required to determine if adventurer coordinates are OK
+        if (!terrain) {
+          throw new TerrainError(
+            'Found adventurer declaration before terrain declaration',
+          );
+        }
+
+        // Check if adventurer is spawned within terrain boundaries
+        if (
+          parsed.x < 0 ||
+          parsed.y < 0 ||
+          parsed.x >= terrain.width ||
+          parsed.y >= terrain.height
+        ) {
+          throw new TerrainError('Adventurer starts out of terrain boundaries');
+        }
+
+        // Check if adventurer starts on a mountain
+        if (mountains.some((m) => m.x === parsed.x && m.y === parsed.y))
+          throw new TerrainError('Adventurer starts on a mountain');
+
+        adventurers.push(parsed);
+      }
+    });
+
+    if (terrain === null)
+      throw new TerrainError('Unable to read terrain data from file');
+
+    // Assemble read data in the single Terrain instance
+    (terrain as Terrain).mountains = mountains;
+    (terrain as Terrain).treasures = treasures;
+    (terrain as Terrain).adventurers = adventurers;
+
+    return terrain;
   }
 
   // eslint-disable-next-line no-unused-vars
   static parseFile(fileName: string): Terrain {
     throw new Error('Not implemented');
   }
+
+  /// /////////////////////////////////////////////////////////
+  //  PRIVATE STATIC METHODS
+  // //////////////////////////////////////////////////////////
 
   /**
    * Parse data from a line and get the right data structure from it
@@ -46,16 +130,16 @@ class Terrain {
     const [lineType, ...lineData] = terrainLine.trim().split(' - ');
 
     switch (lineType) {
-      case 'C':
+      case LineType.Terrain:
         return Terrain.parseTerrain(lineData);
 
-      case 'T':
+      case LineType.Treasure:
         return Terrain.parseTreasures(lineData);
 
-      case 'M':
+      case LineType.Mountain:
         return [Terrain.parseMountain(lineData)];
 
-      case 'A':
+      case LineType.Adventurer:
         return Terrain.parseAdventurer(lineData);
 
       default: {

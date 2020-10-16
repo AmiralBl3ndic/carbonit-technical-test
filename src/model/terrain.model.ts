0a765ce1e39e35149e2edec9137ec61af38a588a
AmiralBl3ndic <camille.briand@efrei.net>
2020-10-16 10:20:20 +0200

    feat(Terrain): start to implement Terrain.parseLine

--- src/model/terrain.model.ts
+++ src/model/terrain.model.ts
@@ -1,3 +1,4 @@
+import TerrainError from '@/error/terrain.error';
 import Tile from '@/model/tile.model';
 
 /**
@@ -35,7 +36,99 @@ class Terrain {
 
   // eslint-disable-next-line no-unused-vars
   private static parseLine(terrainLine: string): Terrain | Tile[] {
-    throw new Error('Not implemented');
+    const [lineType, ...terrainData] = terrainLine.trim().split(' - ');
+
+    switch (lineType) {
+      case 'C': {
+        if (terrainData.length < 2)
+          throw new Error('Too few information for terrain definition');
+
+        if (terrainData.length > 2)
+          throw new TerrainError('Too much information for terrain definition');
+
+        const [width, height] = terrainData.map((_) => Number(_));
+
+        if (![width, height].every(Number.isInteger)) {
+          throw new TerrainError('Unparsable data for terrain definition');
+        }
+
+        if (width <= 0 || height <= 0) {
+          throw new TerrainError('Terrain cannot have 0 height or width');
+        }
+
+        return new Terrain(width, height);
+      }
+
+      case 'T': {
+        if (terrainData.length < 3)
+          throw new TerrainError(
+            'Too few information for tile of type treasure',
+          );
+
+        if (terrainData.length > 3)
+          throw new TerrainError(
+            'Too much information for tile of type treasure',
+          );
+
+        const [x, y, quantity] = terrainData.map((_) => Number(_));
+
+        if ([x, y, quantity].some((_) => !Number.isInteger(_) || _ < 0)) {
+          throw new TerrainError(
+            'Width, height and quantity must be >= 0 for a treasure tile',
+          );
+        }
+
+        return new Array<Tile>(quantity).fill({
+          x,
+          y,
+          type: 'treasure',
+        });
+      }
+
+      case 'M': {
+        if (terrainData.length < 2)
+          throw new TerrainError(
+            'Too few information for tile of type mountain',
+          );
+
+        if (terrainData.length > 2)
+          throw new TerrainError(
+            'Too much information for tile of type mountain',
+          );
+
+        const [x, y] = terrainData.map((_) => Number(_));
+
+        if ([x, y].some((_) => !Number.isInteger(_) || _ < 0)) {
+          throw new TerrainError(
+            'Width and height must be >= 0 for a mountain tile',
+          );
+        }
+
+        return [
+          {
+            x,
+            y,
+            type: 'mountain',
+          },
+        ];
+      }
+
+      case 'A': {
+        if (terrainData.length < 5)
+          throw new TerrainError('Too few information for adventurer');
+
+        if (terrainData.length > 5)
+          throw new TerrainError('Too much information for adventurer');
+
+        return [];
+      }
+
+      default: {
+        throw new TerrainError(
+          `Unable to parse terrain tile with data: "${terrainLine}"`,
+        );
+      }
+    }
   }
 }
 

e8255e09479e952caed51e5bf1fcbe9212d80aea
AmiralBl3ndic <camille.briand@efrei.net>
2020-10-17 01:36:11 +0200

    feat(Adventurer): implement Adventurer to make tests pass

--- src/model/adventurer.model.ts
+++ src/model/adventurer.model.ts
@@ -1,3 +1,4 @@
+import AdventurerError from '@/error/adventurer.error';
 import Action from '@/model/action.enum';
 import Orientation from '@/model/orientation.enum';
 
@@ -17,6 +18,8 @@ class Adventurer {
 
   actions: Action[];
 
+  treasures: number = 0;
+
   constructor(
     name: string,
     startX: number,
@@ -32,11 +35,74 @@ class Adventurer {
   }
 
   move(): void {
-    const a = this.x + this.y;
+    const [nextX, nextY] = this.nextCoords;
+
+    // Check if move would move adventurer out of boundaries
+    if (nextX < 0 || nextY < 0)
+      throw new AdventurerError(
+        'Cannot move adventurer to negative coordinates',
+      );
+
+    // Perform move
+    [this.x, this.y, this.orientation] = this.nextCoords;
+
+    // Get rid of action
+    this.actions.shift();
   }
 
+  /**
+   * Coordinates of the adventurer after he performs its next move
+   */
   get nextCoords(): AdventurerCoords {
-    return [this.x, this.y, this.orientation];
+    let { x, y, orientation } = this;
+
+    const action = this.actions[0];
+
+    if (action === Action.MOVE_FORWARD) {
+      switch (this.orientation) {
+        case Orientation.NORTH:
+          y -= 1;
+          break;
+        case Orientation.SOUTH:
+          y += 1;
+          break;
+        case Orientation.EAST:
+          x += 1;
+          break;
+        default:
+          x -= 1;
+      }
+    } else if (action === Action.TURN_LEFT) {
+      switch (this.orientation) {
+        case Orientation.NORTH:
+          orientation = Orientation.WEST;
+          break;
+        case Orientation.SOUTH:
+          orientation = Orientation.EAST;
+          break;
+        case Orientation.EAST:
+          orientation = Orientation.NORTH;
+          break;
+        default:
+          orientation = Orientation.SOUTH;
+      }
+    } else {
+      switch (this.orientation) {
+        case Orientation.NORTH:
+          orientation = Orientation.EAST;
+          break;
+        case Orientation.SOUTH:
+          orientation = Orientation.WEST;
+          break;
+        case Orientation.EAST:
+          orientation = Orientation.SOUTH;
+          break;
+        default:
+          orientation = Orientation.NORTH;
+      }
+    }
+
+    return [x, y, orientation];
   }
 }
 

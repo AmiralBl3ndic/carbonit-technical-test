420ca8e28222bf1dfab3f693803abfd66176924b
AmiralBl3ndic <camille.briand@efrei.net>
2020-10-18 03:03:05 +0200

    feat: add base script to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('TODO: write program');
+import 'module-alias/register';
+import Terrain from '@/model/terrain.model';
+
+if (process.argv.length <= 2) {
+  console.log('No file to scan');
+  process.exit(0);
+}
+
+const filesToParse = process.argv.slice(-1);
+
+filesToParse.forEach((file: string) => {
+  console.log(`Parsing file ${file}`);
+
+  const terrain = Terrain.parseFile(file);
+
+  terrain.run();
+
+  terrain.saveResultToFile(`${file}.result`);
+
+  console.log('\n==================================\n');
+});
